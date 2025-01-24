ca314d5aa010cb371d948472dc49449930a2d60e
jithjayan <jithjayan9341@gmail.com>
2025-01-24 11:36:24 +0530

    templates

--- rzrprjt/app/views.py
+++ rzrprjt/app/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+from django.conf import settings
+import razorpay
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+def home(request):
+    return render(request,"index.html")
+
+def order_payment(request):
+    if request.method == "POST":
+        name=request.POST.get('name')
+        amount=request.POST.get('amount')
+        clinet=razorpay.Client(auth=(settings.RAZORPAY_KEY_ID,settings.RAZORPAY_KEY_SECRET))
+        razorpay_order=clinet.order.create(
+            {'amount':amount,'currency':'INR','payment_capture':'1'})
+        order_id=razorpay_order['id']
+        order=Order.objects.create(
+            name=name,
+            amount=amount,
+            provider_order_id=order_id
+        )
+        order.save()
+        data={
+                "callback_url":"http://" + "127.0.0.1:8000" + "/razorpay/callback",
+                "razorpay_key":settings.RAZORPAY_KEY_ID,
+                "order":order,
+            }
+        print(data)
+        return render(
+            request,
+            "index.html",data
+        )
+    return render(request,"index.html")
+
+@csrf_exempt
+def callback(request):
+    def verify_signature(respone_data):
+        client=razorpay.Client(auth=(settings.RAZORPAY_KEY_ID,settings.RAZORPAY_KEY_SECRET))
+        return client.utility.verify_payment_signature(respone_data)
+    
+    if "razorpay_signature" in request.POST:
+        payment_id=request.POST.get("razorpay_payment_id","")
+        provider_order_id=request.POST.get("razorpay_order_id","")
+        signature_id=request.POST.get("razorpay_signature","")
+        order=Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id=payment_id 
+        order.signature_id=signature_id
+        order.save()
+        if not verify_signature(request.POST):
+            order.status=PaymentStatus.SUCCESS
+            order.save()
+            return render(request,"callback.html",context={"status":order.status})
+        
+        else:
+            order.status=PaymentStatus.FAILURE
+            order.save()
+            return render(request,"callback.html",context={"status":order.status})
+        
+    else:
+        payment_id=json.loads(request.POST.get("error[metadata]")).get("payment_id")
+        provider_order_id=json.loads(request.POST.get("error[metadata]")).get(
+            "order_id"
+        )
+        order=Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id=payment_id
+        order.status=PaymentStatus.FAILURE
+        order.save()
+        return render(request,"callback.html",context={"status":order.status})
--- rzrprjt/rzrprjt/settings.py
+++ rzrprjt/rzrprjt/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'rzrprjt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+RAZORPAY_KEY_ID= 'rzp_test_CLerpHwzGdtyfl'
+RAZORPAY_KEY_SECRET= 'dzYbbn3gAeTI7tWJ3ycRtijQ'
